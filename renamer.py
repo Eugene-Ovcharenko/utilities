94e08222b3d55527ebc80a0fb8bda7e78e6dd44f
Evgeny Ovcharenko <ov.eugene@gmail.com>
2023-05-12 13:30:45 +0700

    [Feature] renamer.py was modified - sorting by (sheep_#) added

--- renamer.py
+++ renamer.py
@@ -2,6 +2,8 @@ import os
 import re
 import pandas as pd
 
+pd.options.mode.chained_assignment = None
+
 
 def get_directory_size(
         path: str
@@ -16,25 +18,35 @@ def get_directory_size(
     return total_size / 1024
 
 
-path = '/media/nii/SP PHD U3/dataset/source'
+def renamer(
+        path: str,
+) -> None:
+    df = pd.DataFrame(columns=['new_name', 'original_name', 'size', 'path'])
+    for i, folder in enumerate(os.listdir(path)):
+        if os.path.isdir(os.path.join(path, folder)):
+            fsize_mb = get_directory_size(os.path.join(path, folder))
+            fsize_mb = f'{fsize_mb:.0f} Mb'
 
-df = pd.DataFrame(columns=['new_name', 'original_name', 'size', 'path'])
-for i, folder in enumerate(os.listdir(path)):
-    if os.path.isdir(os.path.join(path, folder)):
-        fsize_mb = get_directory_size(os.path.join(path, folder))
-        fsize_mb = f'{fsize_mb:.0f} Mb'
+            fname = re.search(r'sheep\s*(\d+)', folder) or re.search(r'овца\s*(\d+)', folder)
+            if fname:
+                sheep = f'sheep_{int(fname.group(1)):02d}'
+            else:
+                sheep = 'sheep_NA'
 
-        fname = re.search(r'sheep\s+(\d+)', folder) or re.search(r'овца\s+(\d+)', folder)
-        if fname:
-            sheep = f'sheep_{fname.group(1):02d}'
-        else:
-            sheep = 'sheep_NA'
+            df.loc[i] = [sheep, folder, fsize_mb, path]
+    df = df.sort_values(by=['new_name']).reset_index(drop=True)
 
-        new_name = f'{i + 1:03d}_{sheep}'
-        old_name_path = os.path.join(path, folder)
-        new_name_path = os.path.join(path, new_name)
+    for i, row in df.iterrows():
+        new_name = f'{(i + 1):03d}_{row.new_name}'
+        df['new_name'].loc[i] = new_name
+
+        old_name_path = os.path.join(row.path, row.original_name)
+        new_name_path = os.path.join(row.path, row.new_name)
         os.rename(old_name_path, new_name_path)
-        df.loc[i] = [new_name, folder, fsize_mb, path]
+    path = os.path.(path, 'folder_list.xlsx')
+    df.to_excel(path, index=False)
 
-path = os.path.join(path, 'folder_list.xlsx')
-df.to_excel(path, index=False)
+
+if __name__ == '__main__':
+    path = '/media/nii/SP PHD U3/dataset/source'
+    renamer(path=path)
